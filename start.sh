8d4f0c59daab87dc6353683954af2b37ea0cab62
aishit.dharwal <aishit.dharwal@skooc.com>
2025-12-03 11:03:56 +0530

    simplify

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python -c "from health_check import run_health_check_server; run_health_check_server()" &
-streamlit run app.py --server.port=8080 --server.address=0.0.0.0 --server.headless=true
